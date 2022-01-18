100056f1849861aef5f478787b7c8fc6e685df48
valentinaq13 <emeritavalentina@gmail.com>
2022-01-18 13:04:08 -0300

    :)

--- client/src/components/Countries.jsx
+++ client/src/components/Countries.jsx
@@ -12,22 +12,18 @@ function Countries() {
 
     const dispatch = useDispatch()
     const allCountries = useSelector((state) => state.countries)
-    const[orden, setOrden] = useState("")
     const [currentPage, setCurrentPage] = useState(1)//1ro mi pagina actual y un estado q setee mi pag actual
     // const [countriesPerPage, setCountriesPerPage] = useState(10)//setea cant personajes x pag
     var countriesPerPage = 0;
-    if (currentPage === 1){countriesPerPage = 9}
-    if(currentPage >= 2){countriesPerPage = 10 }
+    if (currentPage === 1) { countriesPerPage = 9 }
+    if (currentPage >= 2) { countriesPerPage = 10 }
     const lastCountry = currentPage * countriesPerPage//10
     const firstCountry = lastCountry - countriesPerPage //0
-    const currentCountries = allCountries.slice(firstCountry,lastCountry)//ésta constante tiene los personajes que estan en la pagina actual
-    
-   
+    const currentCountries = allCountries.slice(firstCountry, lastCountry)//ésta constante tiene los personajes que estan en la pagina actual
 
-    const paginado = (pageNumber) =>{
+    const paginado = (pageNumber) => {
         setCurrentPage(pageNumber)
     }
-    
 
     useEffect(() => {
         dispatch(getCountries());
@@ -36,50 +32,34 @@ function Countries() {
     function handleClick(e) {
         e.preventDefault();
         dispatch(getCountries());
-        
+
     }
-    // function handleFilter(e) {
-    //     e.preventDefault()
-    //     dispatch(sortFunction(e.target.value))
-    //     console.log(e.target.value + "HOLAAA")
-    //     // setOrden(e.target.value)
-    //     setCurrentPage(1); 
-    //     setOrden(`Ordenado ${e.target.value}`) //para lo unico que lo uso, es para que me haga la modificacion en el renderizado
-    // }
     return (
         <div >
-            {/* <div>
-            <p>You can order:</p>
-            <select onChange={e => handleFilter(e)}>
-                <option value="asc"> from A to Z</option>
-                <option value="desc"> from Z to A</option>
-                <option value="max">+ Population</option>
-                <option value="min">-Population</option>
-            </select>
-        </div> */}
-        <hr />
-        <div>
-               
-               <button onClick={e => { handleClick(e) }}>Volver a cargar todos los países</button>
-           </div>
-            <div className={styles.cards}>
-            {
-                currentCountries.map(el => {
-                    return (
-                        <fragment>
-                            <Link to={"/home/" + el.id} style={{paddingLeft: 13, textDecoration: 'none', color: 'gray'}}>
-                                <Card name={el.name} flag={el.flag} continent={el.continent} id={el.id} />
-                            </Link>
-                        </fragment>
-                    )
-                })
-            }
+
+            <hr />
+            <div>
+
+                <button onClick={e => { handleClick(e) }}>Volver a cargar todos los países</button>
             </div>
-           
+            <div className={styles.cards}>
+                {
+                    currentCountries.map(el => {
+                        return (
+                            <div key={el.id}>
+                                <Link to={"/home/" + el.id} style={{ paddingLeft: 13, textDecoration: 'none', color: 'gray' }}>
+                                    <Card name={el.name} flag={el.flag} continent={el.continent} />
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
             <Paginado
-            countriesPerPage={countriesPerPage}
-            allCountries={allCountries.length}
-            paginado={paginado}
+                countriesPerPage={countriesPerPage}
+                allCountries={allCountries.length}
+                paginado={paginado}
             />
         </div>
     )
