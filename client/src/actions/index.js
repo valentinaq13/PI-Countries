38581a4b88376a6e649b1c025e2c086e21923736
valentinaq13 <emeritavalentina@gmail.com>
2022-01-18 16:08:22 -0300

    almostfin

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,54 +3,54 @@ export const GET_COUNTRIES = "GET_COUNTRIES";
 export const SEARCH_BY_NAME = "SEARCH_BY_NAME";
 export const FILTER_BY_NAME = "FILTER_BY_NAME";
 export const GET_DETAIL = "GET_DETAIL";
-export const FILTER_CONTINENT= "FILTER_CONTINENT";
-export const GET_ACTIVITIES= "GET_ACTIVITIES";
-export const SORT= "SORT";
-export const FILTER_ACTIVITY="FILTER_ACTIVITY"
+export const FILTER_CONTINENT = "FILTER_CONTINENT";
+export const GET_ACTIVITIES = "GET_ACTIVITIES";
+export const SORT = "SORT";
+export const FILTER_ACTIVITY = "FILTER_ACTIVITY"
 
 
 export function getCountries() {
-    return function (dispatch) {
-      axios.get('http://localhost:3001/countries')
-        .then((resp) => {
-          return dispatch({
-            type: GET_COUNTRIES,
-            payload: resp.data
-          })
-        })
-        .catch((datos) => console.error(datos))
-    }
-  };
-  
-  export function searchByName(name) {
-    return async function (dispatch) {
-      try {
-        let resp = await axios.get(`http://localhost:3001/countries?name=${name}`)
+  return function (dispatch) {
+    axios.get('http://localhost:3001/countries')
+      .then((resp) => {
         return dispatch({
-          type: SEARCH_BY_NAME,
+          type: GET_COUNTRIES,
           payload: resp.data
-        });
-  
-      } catch (error) {
-        console.log(error)
-  
-      }
-    }
+        })
+      })
+      .catch((datos) => console.error(datos))
   }
+};
 
-  export function sortFunction(payload){
-    return {
-      type: SORT,
-      payload
+export function searchByName(name) {
+  return async function (dispatch) {
+    try {
+      let resp = await axios.get(`http://localhost:3001/countries?name=${name}`)
+      return dispatch({
+        type: SEARCH_BY_NAME,
+        payload: resp.data
+      });
+
+    } catch (error) {
+      console.log(error)
+
     }
   }
-  
-  export function filterByContinent(payload){
-    return{
-      type: FILTER_CONTINENT,
-      payload
-    }
+}
+
+export function sortFunction(payload) {
+  return {
+    type: SORT,
+    payload
   }
+}
+
+export function filterByContinent(payload) {
+  return {
+    type: FILTER_CONTINENT,
+    payload
+  }
+}
 export function postActivity(input) {
   return async function () {
     try {
@@ -60,37 +60,26 @@ export function postActivity(input) {
   }
 
 }
-  // export function postActivity(payload){
-  //   return async function(dispatch){
-  //     try{
-  //     const response = await axios.post("http://localhost:3001/activity", payload)
-  //     return dispatch({
-  //       type: POST_ACTIVITY,
-  //       payload: response.data
-  //     })
-  //   } catch(e){console.log(e)}
-  // }
-  // }
-  export function getActivities() {
-    return async function (dispatch) {
-      try {
-        let resul = await axios.get(`http://localhost:3001/activity`);
-        return dispatch({
-          type: GET_ACTIVITIES,
-          payload: resul.data,
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  }
-  
-  export function filterByActivities(payload){
-    return{
-      type: FILTER_ACTIVITY,
-      payload
+export function getActivities() {
+  return async function (dispatch) {
+    try {
+      let resul = await axios.get(`http://localhost:3001/activity`);
+      return dispatch({
+        type: GET_ACTIVITIES,
+        payload: resul.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
+}
+
+export function filterByActivities(payload) {
+  return {
+    type: FILTER_ACTIVITY,
+    payload
   }
+}
 
 export function getDetail(id) {
   return async function (dispatch) {
