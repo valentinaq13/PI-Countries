08e14db44f9be7e84b7c8a233bf4e92d46c03bb8
valentinaq13 <emeritavalentina@gmail.com>
2022-01-19 10:58:38 -0300

    theend

--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -9,17 +9,18 @@ const { getAllCountries } = require("./controllers")
 router.get("/", async (req, res) =>{
     try{
         const name = req.query.name;
+        console.log(name, typeof name)
         let countriesTotal = await getAllCountries();
         if (name){
-            let countryName = await countriesTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase()))
+            let countryName = countriesTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase()))
             countryName.length ?
             res.status(200).send(countryName) :
-            res.status(404).send("Country not founded")
-        } else{
+            res.status(404).send("nonexistent country")
+        } else if(countriesTotal){
             res.status(200).send(countriesTotal)
         }
     }
-    catch(e){res.status(404).send("Country not founded")}
+    catch(e){res.status(404).send("nonexistent country")}
 });
 
 router.get("/:id", async (req, res) => {
@@ -36,9 +37,9 @@ router.get("/:id", async (req, res) => {
         });
             project ?
             res.status(200).json(project) :
-            res.status(404).send("Pais no encontrado")
+            res.status(404).send("nonexistent country")
     }
-    catch (e) {res.status(404).send("Country not founded")}
+    catch (e) {res.status(404).send("nonexistent country")}
 });
 
 module.exports = router;
